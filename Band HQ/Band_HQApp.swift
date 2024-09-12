879ef08ceb5c4b4e92b25c99af02c7b278a54691
Drew Douglass <drew.douglass@hotmail.com>
2024-09-12 18:18:38 -0500

    initial commit

---
+++ Band HQ/Band_HQApp.swift
@@ -0,0 +1,17 @@
+//
+//  Band_HQApp.swift
+//  Band HQ
+//
+//  Created by douglass fam on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Band_HQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Band HQ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Band HQ
+//
+//  Created by douglass fam on 9/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
