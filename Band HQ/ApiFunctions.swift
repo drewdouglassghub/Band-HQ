b4ca200878c51e39b0984cdd0d315034f6c46afe
Drew Douglass <drew.douglass@hotmail.com>
2024-09-14 07:40:12 -0500

    Working sign in to home view

---
+++ Band HQ/ApiFunctions.swift
@@ -0,0 +1,8 @@
+//
+//  ApiFunctions.swift
+//  Band HQ
+//
+//  Created by douglass fam on 9/12/24.
+//
+
+import Foundation
---
+++ Band HQ/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Band HQ
+//
+//  Created by douglass fam on 9/12/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
